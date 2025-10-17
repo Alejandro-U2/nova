ab8d8df6df32f533e6cb20a8612ccac396f3abde
Alejandro-U2 <jeffryurbina73@gmail.com>
2025-10-17 08:43:16 -0600

    prueba

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,10 @@ const PORT = process.env.PORT || 5000;
 
 require("dotenv").config({ path: path.join(__dirname, '.env') });
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://18.222.143.196:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 // ----------Rutas
